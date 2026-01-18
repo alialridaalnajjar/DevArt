abeb9c1036ec0f241239d3d71575ad03c2247faf
alialridaalnajjar <alialridaalnajjar@gmail.com>
2026-01-18 18:01:15 +0200

    Reorder user routes in Data.route.ts for improved readability

--- routes/Data.route.ts
+++ routes/Data.route.ts
@@ -8,8 +8,9 @@ router.delete("/delete/:userId", UserController.deleteUser);
 router.put("/admin/edit/:userId", UserController.editUserDataById);
 
 // User routes--
-router.get("/:userId", UserController.getProfile);
 router.get("/skills/:userId", UserController.getSkills);
-router.put("/edit/:userId", UserController.editProfile);
 router.put("/skills/edit/:userId", UserController.addSkills);
+router.put("/edit/:userId", UserController.editProfile);
+router.get("/:userId", UserController.getProfile);
+
 export default router;
